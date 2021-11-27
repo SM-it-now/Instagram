047fce8f53214a0ca54913891eae902db5f185d3
iseungmin <ibb09@naver.com>
2021-11-28 02:18:03 +0900

    21.11.28 회원가입 페이지 구현 및 특정상황의 로그인, 회원가입 불가능하게 구현.

--- apis/views.py
+++ apis/views.py
@@ -13,7 +13,9 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 # CBV 기반의 api 구현
+@method_decorator(csrf_exempt, name='dispatch')
 class BaseView(View):
+
     @staticmethod
     def response(data={}, message='', status=200):
         result = {
@@ -21,15 +23,12 @@ class BaseView(View):
             'message': message,
 
         }
-        return JsonResponse(result, status)
+        return JsonResponse(result, status=status)
+
+    # 유저 생성 및 예외처리 구현
 
 
-# 유저 생성 및 예외처리 구현
 class UserCreateView(BaseView):
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
-
     # post 입력 처리 및 검증 구현
     def post(self, request):
         username = request.POST.get('username', '')
@@ -39,17 +38,14 @@ class UserCreateView(BaseView):
         if not password:
             return self.response(message='비밀번호를 입력해 주세요.', status=400)
         email = request.POST.get('email', '')
-        if not email:
+        try:
+            validate_email(email)
+        except ValidationError:
             return self.response(message='이메일을 입력해 주세요.', status=400)
-        else:
-            try:
-                validate_email(email)
-            except ValidationError:
-                self.response(message='올바른 이메일을 입력해 주세요.', status=400)
 
         # 예외 처리
         try:
-            user = User.objects.create_user(username, password, email)
+            user = User.objects.create_user(username, email, password)
         except IntegrityError:
             return self.response(message='이미 존재하는 아이디 입니다.', status=400)
 
@@ -69,7 +65,7 @@ class UserLoginView(BaseView):
         # authenticate 함수는 username, password이 일치하지 않을경우, None을 반환.
         user = authenticate(request, username=username, password=password)
         if user is None:
-            return self.response(request, message='아이디 또는 비밀번호가 일치하지 않습니다.', status=400)
+            return self.response(message='아이디 또는 비밀번호가 일치하지 않습니다.', status=400)
         login(request, user)
 
         return self.response()
@@ -77,7 +73,5 @@ class UserLoginView(BaseView):
 
 class UserLogoutView(BaseView):
     def get(self, request):
-        logout(self.request)
+        logout(request)
         return self.response()
-
-
