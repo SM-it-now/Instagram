213f367c951132587dd01ce71ebf00fde88c6752
iseungmin <ibb09@naver.com>
2022-01-04 15:09:33 +0900

    22.01.04 docker 기본 셋팅 및 이사 완료

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,38 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.shortcuts import redirect
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-from contents.views import HomeView, RelationView
-
-class NonUserTemplateView(TemplateView):
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_anonymous:
-            return redirect('contents_home')
-        return super(NonUserTemplateView, self).dispatch(request, *args, **kwargs)
-
+from django.urls import path
 
 urlpatterns = [
-    # 베이스
     path('admin/', admin.site.urls),
-    path('apis/', include('apis.urls')),
-    path('', HomeView.as_view(), name='contents_home'),
-
-    # 인증
-    path('login/', NonUserTemplateView.as_view(template_name='login.html'), name='login'),
-    path('register/', NonUserTemplateView.as_view(template_name='register.html'), name='register'),
-    path('logout/', NonUserTemplateView.as_view(), name='logout'),
-
-    # 팔로우
-    path('relation/', RelationView.as_view(), name='relation'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
